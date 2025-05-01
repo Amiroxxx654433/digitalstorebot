4494437d2df3bf106ef2ce814fb70bcbe11d64d8
Amiroxxx654433 <138411669+Amiroxxx654433@users.noreply.github.com>
2025-05-01 01:19:41 +0100

    Create config.py

---
+++ config.py
@@ -0,0 +1,15 @@
+# config.py
+
+BOT_TOKEN = "7620831538:AAHDF4rvhKK1sLkhUF_2xlmoYy1r0tQxIPs"
+ADMIN_IDS = [5231298684]  # ضع هنا معرف الأدمن (ID) أو أكثر
+
+BINANCE_PAY_INSTRUCTIONS = """
+للدفع عبر Binance Pay:
+- افتح تطبيق Binance
+- اذهب إلى Binance Pay
+- امسح كود الدفع أو أدخل Pay ID
+- بعد الدفع اضغط على تأكيد.
+"""
+
+LANGUAGES = ["ar", "en"]
+DEFAULT_LANGUAGE = "ar"
