7dcd0c609df9efe730d6290d9852565e9bf4a0ec
Amiroxxx654433 <138411669+Amiroxxx654433@users.noreply.github.com>
2025-05-01 15:41:58 +0100

    Update config.py

--- config.py
+++ config.py
@@ -2,9 +2,9 @@ import os
 
 BOT_TOKEN = os.getenv("7620831538:AAHDF4rvhKK1sLkhUF_2xlmoYy1r0tQxIPs")  # التوكن الخاص بالبوت
 ADMIN_ID = int(os.getenv("5231298684"))  # معرف الأدمن
-BINANCE_API_KEY = os.getenv("BINANCE_API_KEY")  # مفتاح Binance Pay
-BINANCE_API_SECRET = os.getenv("BINANCE_API_SECRET")
-BINANCE_MERCHANT_ID = os.getenv("BINANCE_MERCHANT_ID")
+BINANCE_API_KEY = os.getenv("xkVMB2fl6EZRj1LtnPf27I8cWEYuZ02sTmudPn8Y1naF0ld748Z56R7kdeOWueoB")  # مفتاح Binance Pay
+BINANCE_API_SECRET = os.getenv("uNGingZY4BpKddtJ32ed1pOcep32jAQ38fknJNVTu0Cu8YyTJRtgmPip5gjidMLA")
+BINANCE_MERCHANT_ID = os.getenv("394750215")
 
 LANGUAGES = ["en", "ar", "fr"]  # اللغات المدعومة
 DEFAULT_LANGUAGE = "en"  # اللغة الافتراضية
